4f9d9df67d2c8af9205a04018851d12d842b1a9d
tunv <tunguyen060811@gmail.com>
2022-09-25 16:55:32 +0700

    feat: add sign up page

--- web_user/main/views.py
+++ web_user/main/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, HttpResponse
 
+from .forms import RegistrationForm
+
 # Create your views here.
 
 
 def home(request):
     return render(request, 'main/home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'registration/sign_up.html', context)
