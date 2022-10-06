0eab006e99ebb61a6d004799f64f5c7e9f26f0ab
tunv <tunguyen060811@gmail.com>
2022-10-07 00:00:28 +0700

    feat: working with user login logout

--- web_user/main/views.py
+++ web_user/main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import login
 
 from .forms import RegistrationForm
 
@@ -11,6 +12,10 @@ def home(request):
 def sign_up(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
     else:
         form = RegistrationForm()
 
