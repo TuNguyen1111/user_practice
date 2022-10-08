1a032f76f6e4ad0e3d3369d7c69b0848d1b838a2
tunv <tunguyen060811@gmail.com>
2022-10-08 09:48:53 +0700

    add login required

--- web_user/main/views.py
+++ web_user/main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 from .forms import RegistrationForm
 
 # Create your views here.
 
-
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'main/home.html')
 
